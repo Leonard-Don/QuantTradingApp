b53780a5b7ab8a238fd0dbe0735d92a7cc476bee
Leonard-Don <1916637230@qq.com>
2026-05-14 11:12:59 +0800

    build(android): add baseline profile benchmark skeleton (#5)
    Co-authored-by: leo <leonarddon266@gmail.com>

--- TianXianQuant/build.gradle.kts
+++ TianXianQuant/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("com.android.application") version "8.6.1" apply false
+    id("com.android.test") version "8.6.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("org.jetbrains.kotlin.kapt") version "1.9.22" apply false
+    id("androidx.baselineprofile") version "1.3.3" apply false
 }
